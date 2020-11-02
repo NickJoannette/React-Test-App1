8e7bea326cb4248a9a8f464caf18aac24589e3d1
NickJoannette <nicholas.joannette@gmail.com>
2020-11-01 23:14:25 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Test-App1/precache-manifest.144f0a35f02378579b32dacd6f17c556.js"
+  "/React-Test-App1/precache-manifest.cceb24afd96f69d288d2c8e53e907e4e.js"
 );
 
 self.addEventListener('message', (event) => {
